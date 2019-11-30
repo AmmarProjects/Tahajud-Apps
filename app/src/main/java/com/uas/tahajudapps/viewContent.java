db267c577f169c7579263c645f9f35eba390cf52
Muhammad Ammarullah R <muhammadammarullah14@gmail.com>
2019-12-01 06:28:25 +0700

    Update Content

---
+++ app/src/main/java/com/uas/tahajudapps/viewContent.java
@@ -0,0 +1,13 @@
+package com.uas.tahajudapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewContent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_content);
+    }
+}
