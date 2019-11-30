8cf5c1c714eeff461f3fc614eb3198eaf1033e2e
Muhammad Ammarullah R <muhammadammarullah14@gmail.com>
2019-12-01 06:28:38 +0700

    Update Content

--- app/src/main/java/com/uas/tahajudapps/adapter/crud_contentAdapter.java
+++ app/src/main/java/com/uas/tahajudapps/adapter/crud_contentAdapter.java
@@ -6,13 +6,17 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.uas.tahajudapps.R;
 import com.uas.tahajudapps.modal.Content;
+import com.uas.tahajudapps.viewContent;
 
 import java.util.ArrayList;
 
@@ -35,10 +39,23 @@ public class crud_contentAdapter extends RecyclerView.Adapter<crud_contentAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull crudContentViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull crudContentViewHolder holder, final int position) {
         final Content data = dataList.get(position);
         holder.tvSubtitle.setText(dataList.get(position).getSubtitle());
         holder.tvBody.setText(dataList.get(position).getBody());
+
+        holder.btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, viewContent.class);
+                intent.putExtra("key",dataList.get(position).getId());
+                intent.putExtra("subtitle",dataList.get(position).getSubtitle());
+                intent.putExtra("body",dataList.get(position).getBody());
+                intent.putExtra("category",dataList.get(position).getCategory());
+                context.startActivity(intent);
+                Toast.makeText(context, "You Clicked "+dataList.get(position).getId(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -48,11 +65,13 @@ public class crud_contentAdapter extends RecyclerView.Adapter<crud_contentAdapte
 
     public class crudContentViewHolder extends RecyclerView.ViewHolder {
         private TextView tvSubtitle, tvBody;
+        private Button btnView;
 
         public crudContentViewHolder(@NonNull View itemView) {
             super(itemView);
             tvSubtitle = (TextView)itemView.findViewById(R.id.subtitle_admin);
             tvBody = (TextView)itemView.findViewById(R.id.body_admin);
+            btnView = (Button)itemView.findViewById(R.id.btn_view);
         }
     }
 }
--- app/src/main/java/com/uas/tahajudapps/viewContent.java
+++ app/src/main/java/com/uas/tahajudapps/viewContent.java
@@ -1,13 +1,113 @@
 package com.uas.tahajudapps;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class viewContent extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.uas.tahajudapps.conf.Controller;
+import com.uas.tahajudapps.conf.config;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class viewContent extends AppCompatActivity implements View.OnClickListener {
+    private EditText edtTitle;
+    private EditText edtContent;
+
+    private Button buttonUpdate;
+    private Button buttonDelete;
+    String id, subtitle, body, category;
+
+    ProgressDialog pd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_content);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            this.id = extras.getString("key");
+            this.subtitle = extras.getString("subtitle");
+            this.body = extras.getString("body");
+            this.category = extras.getString("category");
+        }
+
+        edtTitle = (EditText) findViewById(R.id.edt_title_content);
+        edtContent = (EditText) findViewById(R.id.edt_content);
+
+        buttonUpdate = (Button) findViewById(R.id.btn_update_content);
+        buttonDelete = (Button) findViewById(R.id.btn_delete_content);
+
+        edtTitle.setText(subtitle);
+        edtContent.setText(body);
+
+        buttonDelete.setOnClickListener(this);
+        buttonUpdate.setOnClickListener(this);
+    }
+
+    private void updateContent()
+    {
+        StringRequest updateReq = new StringRequest(Request.Method.POST, config.URL_UPDATE_CONTENT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject res = new JSONObject(response);
+                            Toast.makeText(viewContent.this ,"Data berhasil diupdate" , Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Intent intent = new Intent(viewContent.this, listContent.class);
+                        intent.putExtra("key",category);
+                        startActivity(intent);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pd.cancel();
+                        Toast.makeText(viewContent.this, "pesan : Gagal Update Data", Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>();
+                map.put("id",id);
+                map.put("subtitle_content",edtTitle.getText().toString().trim());
+                map.put("body_content",edtContent.getText().toString().trim());
+                map.put("id_category",category);
+
+                return map;
+            }
+        };
+
+        Controller.getInstance().addToRequestQueue(updateReq);
+    }
+
+    private void deleteContent(){
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_update_content){
+            updateContent();
+        }else if(v.getId() == R.id.btn_delete_content){
+            deleteContent();
+        }
     }
 }
